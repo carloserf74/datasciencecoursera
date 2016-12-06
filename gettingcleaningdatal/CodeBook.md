d1bf8635c61efb8dd5335a8de21392f34f519aa8
Carlos Eduardo Reis Ferreira <carloserf@gmail.com>
2016-12-05 22:07:23 -0200

    Add files via upload

---
+++ gettingcleaningdatal/CodeBook.md
@@ -0,0 +1,49 @@
+##CodeBook
+
+Human Activity Recognition Using Smartphones Data Set
+
+_Abstract_: Human Activity Recognition database built from the recordings of 30 subjects performing activities of daily living (ADL) while carrying a waist-mounted smartphone with embedded inertial sensors.
+
+| *Data Set Characteristics:*   | Multivariate, Time-Series  | *Number of Instances:*  | 10299 | *Area:*               | Computer   |
+|-------------------------------|----------------------------|-------------------------|-------|-----------------------|------------|
+| *Attribute Characteristics:*  | N/A                        | *Number of Attributes:* | 561   | *Date Donated:*       | 2012-12-10 |
+|-------------------------------|----------------------------|-------------------------|-------|-----------------------|------------|
+| *Associated Tasks:*           | Classification, Clustering | *Missing Values?*       | N/A   | *Number of Web Hits:* | 420836     |
+
+This is a code book that describes the variables, the data, and any transformations or work that you performed to clean up the data.
+
+*The data source
+
+*Data:* [https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip] (https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
+
+*Description of dataset:* [http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones] (http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+
+#Data Set Information
+
+The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope, we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The experiments have been video-recorded to label the data manually. The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data. 
+
+The sensor signals (accelerometer and gyroscope) were pre-processed by applying noise filters and then sampled in fixed-width sliding windows of 2.56 sec and 50% overlap (128 readings/window). The sensor acceleration signal, which has gravitational and body motion components, was separated using a Butterworth low-pass filter into body acceleration and gravity. The gravitational force is assumed to have only low frequency components, therefore a filter with 0.3 Hz cutoff frequency was used. From each window, a vector of features was obtained by calculating variables from the time and frequency domain.
+
+##Attribute Information:
+
+For each record in the dataset it is provided: 
+
+* Triaxial acceleration from the accelerometer (total acceleration) and the estimated body acceleration. 
+* Triaxial Angular velocity from the gyroscope. 
+* A 561-feature vector with time and frequency domain variables. 
+* Its activity label. 
+* An identifier of the subject who carried out the experiment.
+
+*The dataset includes:*
+
+* features_info.txt: Show 561 features vector
+* features.txt: List all of features
+* activity_labels.txt: Show 6 activity name
+* train/X_train.txt: Show 7352 training observations of the 561 features
+* subject_train.txt: Show 7352 IDs of the volunteers. Its range is from 1 to 30
+* train/y_train.txt: Show 7352 training labels 
+* test/X_test.txt:  Show 2947 test observations of the 561 features
+* subject_test.txt: Show 2947 IDs of the volunteers. Its range is from 1 to 30
+* test/y_test.txt': Show 2947 test labels
+
+##Check the README.txt file for further details about this dataset. 
---
+++ gettingcleaningdatal/run_analysis.R
@@ -0,0 +1,95 @@
+## Author: Carlos Reis - 12/04/2016
+## Final Project of Coursera Course Getting and Cleaning Data
+## Goal:  Collect, Clean and work with data set to prepare a tidy data that can be used for later analyses
+## Data: Collected from the accelerometers from the Samsung Galaxy S smartphone
+## Data Information: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+## Data Repository: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+#######################################################################################################
+## --- 1. Merges the training and the test sets to create one data set ----------------------------- ##
+#######################################################################################################
+
+# Loading the package for use with function melt()
+library(reshape2)
+
+address <- paste(getwd(), "/datasciencecoursera/gettingdata/week4project", sep = "")
+
+# Setting my working directory 
+setwd(address)
+
+# Loading labels with their activity names
+activityLabels <- read.table("./dataset/activity_labels.txt")
+
+# Loading all the features - Metadata
+features <- read.table("./dataset/features.txt")
+
+# Loading the test set
+xTest <- read.table("./dataset/test/X_test.txt") # test data 
+yTest <- read.table("./dataset/test/y_test.txt") # test labels
+subjectTest <- read.table("./dataset/test/subject_test.txt") # identifies the subject of the activity
+
+# Loading the training set
+xTrain <- read.table("./dataset/train/X_train.txt") # training data
+yTrain <- read.table("./dataset/train/y_train.txt") # training labels
+subjectTrain <- read.table("./dataset/train/subject_train.txt") # identifies the subject of the activity
+
+# Adding column name for data sets
+names(xTest) <- features$V2
+names(xTrain) <- features$V2
+names(subjectTest) <- "subjectID"
+names(subjectTrain) <- "subjectID"
+names(yTest) <- "activity"
+names(yTrain) <- "activity"
+
+# Creating a unique data set
+test <- cbind(subjectTest, yTest, xTest)
+train <- cbind(subjectTrain, yTrain, xTrain)
+
+train_and_test <- rbind(train, test) # training and test
+
+#######################################################################################################
+## --- 2. Extracts only the measurements on the mean and standard deviation for each measurement --- ##
+#######################################################################################################
+
+# Define which columns contain mean or std
+measurements <- grepl("mean\\(\\)", names(train_and_test)) | grepl("std\\(\\)", names(train_and_test))
+
+# Keeping only the column subjectID and activity
+measurements[1:2] <- TRUE
+
+# Removing the other columns
+train_and_test <- train_and_test[, measurements]
+
+#######################################################################################################
+## --- 3. Uses descriptive activity names to name the activities in the data set ------------------- ##
+#######################################################################################################
+
+# Done in the data set samsung_train_test_tidy.csv
+
+#######################################################################################################
+## --- 4. Appropriately labels the data set with descriptive variable names ------------------------ ##
+#######################################################################################################
+
+# Setting a name of activities
+activityLabelsLower <- tolower(activityLabels$V2) # Lowering the characterr of activities
+activityLabelsLower2 <- sub("_", "", activityLabelsLower) # Replacement of the "_" for "" 
+
+train_and_test$activity <- factor(train_and_test$activity, labels = c(activityLabelsLower[1], 
+                                                                      activityLabelsLower[2], 
+                                                                      activityLabelsLower[3], 
+                                                                      activityLabelsLower[4], 
+                                                                      activityLabelsLower[5], 
+                                                                      activityLabelsLower[6]))
+
+#######################################################################################################
+## --- 5. From the data set in step 4, creates a second, independent tidy data set with the average  ## 
+## --- of each variable for each activity and each subject. ---------------------------------------- ##
+#######################################################################################################
+
+fusionData <- melt(train_and_test, id = c("subjectID","activity"))
+
+# Creating a tidy data set
+samsung_train_test_tidy <- dcast(fusionData, subjectID + activity ~ variable, mean)
+
+# Writing a file
+write.csv(samsung_train_test_tidy, "samsung_train_test_tidy.csv", row.names=FALSE)
